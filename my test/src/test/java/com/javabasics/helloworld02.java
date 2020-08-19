76f075ebc5c5ca2a65e16faceb9b9b7093c6c98e
edmundocossiodoc <edmundo.cossio.doc@gmail.com>
2020-08-19 13:55:31 -0700

    first commit

---
+++ my test/src/test/java/com/javabasics/helloworld02.java
@@ -0,0 +1,11 @@
+package com.javabasics;
+
+public class helloworld02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("helooo");
+	}
+
+}
